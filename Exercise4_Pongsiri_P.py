f992b26df843d97c100a1cd1852ea43d20bfe9ae
Brian1248 <145556297+Brian1248@users.noreply.github.com>
2023-09-23 18:13:08 +0700

    Create Exercise4_Pongsiri_P.py
    exercise4

---
+++ Exercise4_Pongsiri_P.py
@@ -0,0 +1,10 @@
+FoundEng = 78
+GenBus = 87.8
+ITCS = 80
+ComPro = 81
+
+print("--- Your Score ---")
+print("Foundation English :",FoundEng,"คะแนน")
+print("General Business :",GenBus,"คะแนน")
+print("Introduction to Computer Systems :",ITCS,"คะแนน")
+print("Computer Programming :",ComPro,"คะแนน")
