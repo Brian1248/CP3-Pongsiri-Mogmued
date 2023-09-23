fca27d76329a5a154d933290ed71a472ba84d5cf
Brian1248 <145556297+Brian1248@users.noreply.github.com>
2023-09-23 18:16:50 +0700

    Update Exercise4_Pongsiri_P.py

--- Exercise4_Pongsiri_P.py
+++ Exercise4_Pongsiri_P.py
@@ -3,8 +3,9 @@ GenBus = 87.8
 ITCS = 80
 ComPro = 81
 
-print("--- Your Score ---")
+print("------------ Your Score ------------")
 print("Foundation English :",FoundEng,"คะแนน")
 print("General Business :",GenBus,"คะแนน")
 print("Introduction to Computer Systems :",ITCS,"คะแนน")
 print("Computer Programming :",ComPro,"คะแนน")
+print("------------------------------------")
