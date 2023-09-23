9895196c3486f505b96daf194adef0f765984aed
Brian1248 <145556297+Brian1248@users.noreply.github.com>
2023-09-23 23:45:41 +0700

    Exercise5_1_Pongsiri_M

---
+++ Exercise5_1_Pongsiri_M.py
@@ -0,0 +1,13 @@
+num1 = int(input("1st number?: "))
+num2 = int(input("2nd number?: "))
+
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+
+
+print(num1, "+", num2, "=", result1)
+print(num1, "-", num2, "=", result2)
+print(num1, "*", num2, "=", result3)
+print(num1, "/", num2, "=", int(result4))
---
+++ Exercise5_2_Pongsiri_M.py
@@ -0,0 +1,4 @@
+s = int(input("How far is the travel distance?(km) : "))
+t = int(input("How long does it take to travel?(hour) : "))
+v = s / t
+print(int(v),"km/h")
