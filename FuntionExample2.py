21f65d3e9e473a2e7020576b1c3b3f8787548e78
Brian1248 <145556297+Brian1248@users.noreply.github.com>
2023-09-25 19:39:06 +0700

    Create FuntionExample2.py

---
+++ FuntionExample2.py
@@ -0,0 +1,6 @@
+def addNumbers(x,y):
+    print(x,"+",y,"=",(x+y))
+    print(x,"-",y,"=",(x-y))
+    print(x,"*",y,"=",(x*y))
+    print(x,"/",y,"=",(x/y))
+addNumbers(int(input("Number: ")),int(input("Number: ")))
